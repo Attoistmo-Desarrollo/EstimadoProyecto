353c126cc842d98c831894f194061f8844055c74
Yisus <jesuss240291@gmail.com>
2020-08-11 16:23:33 -0500

    Cambios Jesus 11 Agosto

--- src/app/cronograma-actividades/cronograma-actividades.component.ts
+++ src/app/cronograma-actividades/cronograma-actividades.component.ts
@@ -1,4 +1,4 @@
-
+import { CronogramaActividadesComponenteService } from './shared/cronograma-actividades-componente.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,120 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cronograma-actividades.component.css']
 })
 export class CronogramaActividadesComponent implements OnInit {
+  cronAct: any;
 
-  constructor() {}
-  filterPost = '';
-  cronogramaActividades = [
-    {
-      id: 1,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 2,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 3,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 4,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 5,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 6,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 7,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 8,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 9,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 10,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 11,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-    {
-      id: 12,
-      actividad: '5w',
-      empleado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '15 min',
-      estado: 'En curso',
-      fecha: '02/04/2019'
-    },
-  ];
-    ngOnInit() {
+  constructor(public croActService: CronogramaActividadesComponenteService) { }
+
+  ngOnInit() {
+    this.croActService.getCroAct().subscribe(data => {
+      this.cronAct = data;
+    });
   }
-
 }
--- src/app/cronograma/cronograma.component.ts
+++ src/app/cronograma/cronograma.component.ts
@@ -1,4 +1,4 @@
-
+import { CronogramaComponenteService } from './shared/cronograma-componente.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,132 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cronograma.component.css']
 })
 export class CronogramaComponent implements OnInit {
+  cronograMA: any;
 
-  constructor() { }
-  cronograma = [
-    {
-      id: 1,
-      actividad: '5w',
-      Asignado: 'Aldrid Jesus M',
-      cantidad: 3,
-      tiempo: '10 min',
-      tiempo_real: '11 min',
-      estado: 'en curso',
-      Fecha_entrega: '10-02-2020'
-    },
-    {
-      id: 2,
-      actividad: '5w',
-      Asignado: 'Jesus M',
-      cantidad: 2,
-      tiempo: '12 min',
-      tiempo_real: '11 min',
-      estado: 'en curso',
-      Fecha_entrega: '11-02-2020'
-    },
-    {
-      id: 3,
-      actividad: '5w',
-      Asignado: 'Aldrid Martinez',
-      cantidad: 10,
-      tiempo: '15 min',
-      tiempo_real: '15 min',
-      estado: 'en curso',
-      Fecha_entrega: '12-02-2020'
-    },
-    {
-      id: 4,
-      actividad: '5p',
-      Asignado: 'Genaro Cruz Argueta',
-      cantidad: 11,
-      tiempo: '15 min',
-      tiempo_real: '15 min',
-      estado: 'en curso',
-      Fecha_entrega: '12-02-2019'
-    },
-    {
-      id: 5,
-      actividad: 'Como como',
-      Asignado: 'Abraham Rodriguez N',
-      cantidad: 10,
-      tiempo: '15 min',
-      tiempo_real: '18 min',
-      estado: 'Resuelta',
-      Fecha_entrega: '12-04-2020'
-    },
-    {
-      id: 6,
-      actividad: 'Como Como',
-      Asignado: 'Carlos Sandoval',
-      cantidad: 2,
-      tiempo: '15 min',
-      tiempo_real: '45 min',
-      estado: 'Cerrada',
-      Fecha_entrega: '12-02-2020'
-    },
-    {
-      id: 7,
-      actividad: 'Desarrollo CRUD',
-      Asignado: 'Eduardo Lopez',
-      cantidad: 1,
-      tiempo: '245 min',
-      tiempo_real: '240 min',
-      estado: 'Resuelta',
-      Fecha_entrega: '12-03-2020'
-    },
-    {
-      id: 8,
-      actividad: 'Prueba de Servicios REST',
-      Asignado: 'Samuel Gonzalez',
-      cantidad: 10,
-      tiempo: '15 min',
-      tiempo_real: '15 min',
-      estado: 'cerrada',
-      Fecha_entrega: '12-02-2020'
-    },
-    {
-      id: 9,
-      actividad: 'Desarrollo CRUD',
-      Asignado: 'Eduardo Lopez',
-      cantidad: 1,
-      tiempo: '245 min',
-      tiempo_real: '240 min',
-      estado: 'Resuelta',
-      Fecha_entrega: '12-03-2020'
-    },
-    {
-      id: 10,
-      actividad: 'Prueba de Servicios REST',
-      Asignado: 'Samuel Gonzalez',
-      cantidad: 10,
-      tiempo: '15 min',
-      tiempo_real: '15 min',
-      estado: 'cerrada',
-      Fecha_entrega: '12-02-2020'
-    },
-    {
-      id: 11,
-      actividad: 'Desarrollo CRUD',
-      Asignado: 'Eduardo Lopez',
-      cantidad: 1,
-      tiempo: '245 min',
-      tiempo_real: '240 min',
-      estado: 'Resuelta',
-      Fecha_entrega: '12-03-2020'
-    },
-    {
-      id: 12,
-      actividad: 'Prueba de Servicios REST',
-      Asignado: 'Samuel Gonzalez',
-      cantidad: 10,
-      tiempo: '15 min',
-      tiempo_real: '15 min',
-      estado: 'cerrada',
-      Fecha_entrega: '12-02-2020'
-    }
-  ];
+  constructor(public cronogramaService: CronogramaComponenteService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.cronogramaService.getCronograma().subscribe(data => {
+      this.cronograMA = data;
+    });
   }
-
 }
